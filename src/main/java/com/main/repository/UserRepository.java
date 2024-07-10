505c8ef34a29cb6941331519a89299a3f5dc56e7
Aarish khan <146206625+Aarish-khan91@users.noreply.github.com>
2024-07-10 12:45:07 +0530

    Annotation added.

--- src/main/java/com/main/repository/UserRepository.java
+++ src/main/java/com/main/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.main.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.main.entity.UserModel;
-
+@Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
 
 	Optional<UserModel> findByLoginAndPassword(String login, String password);
